e048f3f6c04edde730e8644e2de1fa9e51af9850
Kate_Farhut <katyfar2008@mail.ru>
2025-08-27 19:29:55 +0300

    commit

--- src/main.tsx
+++ src/main.tsx
@@ -6,7 +6,5 @@ import App from './App';
 const root = createRoot(document.getElementById('root') as HTMLElement);
 
 root.render(
-    <StrictMode>
         <App />
-    </StrictMode>
 );
--- src/types.ts
+++ src/types.ts
@@ -1,6 +1,6 @@
 export interface Task {
     id: number;
-    name: string;
+    text: string;
     completed: boolean;
 }
 
